79b186b6ebdc43a66974496f22d20797a3a99f27
Ma4713 <ma4710320@gmail.com>
2026-04-06 19:20:29 +0100

    Add final logo branding and refine Study Buddy UI

--- src/app.js
+++ src/app.js
@@ -4,19 +4,14 @@ const connection = require("./db/connection");
 
 const app = express();
 
-// PUG setup
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "../views"));
-
-// Static files
 app.use(express.static(path.join(__dirname, "../public")));
 
-// Home route
 app.get("/", (req, res) => {
   res.render("index", { title: "Home", activePage: "home" });
 });
 
-// Users route
 app.get("/users", (req, res) => {
   connection.query("SELECT * FROM users", (err, results) => {
     if (err) {
@@ -32,7 +27,6 @@ app.get("/users", (req, res) => {
   });
 });
 
-// User profile route
 app.get("/users/:id", (req, res) => {
   const userId = req.params.id;
 
@@ -64,7 +58,6 @@ app.get("/users/:id", (req, res) => {
   );
 });
 
-// Sessions route
 app.get("/sessions", (req, res) => {
   connection.query(
     `
@@ -79,8 +72,6 @@ app.get("/sessions", (req, res) => {
         return res.send("Error fetching sessions");
       }
 
-      console.log("SESSIONS RESULTS:", results);
-
       res.render("sessions", {
         sessions: results,
         title: "Sessions",
@@ -90,13 +81,12 @@ app.get("/sessions", (req, res) => {
   );
 });
 
-// Session detail route
 app.get("/sessions/:id", (req, res) => {
   const sessionId = req.params.id;
 
   connection.query(
     `
-    SELECT sessions.*, users.name AS mentor_name, subjects.name AS subject_name
+    SELECT sessions.*, users.name AS mentor_name, users.id AS mentor_user_id, subjects.name AS subject_name
     FROM sessions
     JOIN users ON sessions.mentor_id = users.id
     JOIN subjects ON sessions.subject_id = subjects.id
@@ -122,9 +112,6 @@ app.get("/sessions/:id", (req, res) => {
   );
 });
 
-// Start server
 app.listen(3000, () => {
   console.log("Study Buddy running on http://localhost:3000");
 });
-
-
